e73d668a5819be57fe2deed3a145cfd6c8df3a11
KundaNgambi <kundasantiagongambi@gmail.com>
2024-09-15 22:14:31 +0200

    classes

--- User_profile.py
+++ User_profile.py
@@ -1,12 +1,16 @@
+import phonenumbers as pn
+from phonenumbers import geocoder, carrier
+import pycountry
+
 class Users:
-    def __init__(self, first_name, middle_name, last_name, age, country_code, phone_number, email):
+    def __init__(self, first_name, middle_name, last_name, age, phone_number, country, email):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
         self.age = age
         self.phone_number = phone_number
         self.email = email
-        self.country_code = country_code
+        self.country = country
 
     def users_name(self):
         if self.middle_name.title() == "None":
@@ -19,26 +23,36 @@ class Users:
     def users_age(self):
         print("Age: " + self.age + " years")
 
+    def users_country(self):
+        print("Country: " + self.country.title())
     def users_phone_number(self):
-        print("Phone Number: " + self.country_code + self.phone_number)
+        try:
+            country_data = pycountry.countries.get(name=self.country)
+            if country_data:
+                country_code = pn.country_code_for_region(country_data.alpha_2)
+                phone_number_obj = pn.parse(f"+{country_code}{self.phone_number}")
+                formatted_number = pn.format_number(phone_number_obj, pn.PhoneNumberFormat.INTERNATIONAL)
+                print("Phone Number: " + formatted_number)
+            else:
+                print("Invalid country name.")
+        except Exception as e:
+            print(f"Error processing phone number: {e}")
 
     def users_email(self):
         print("Email: " + self.email)
-
 
 #user_input
 user_first_name = input("Enter first name: ")
 user_middle_name = input("Enter middle name(Enter None if none given): ")
 user_last_name = input("Enter last name: ")
 user_age = input("Enter your age: ")
-user_country_code = input("Enter your country code: ")
+user_country = input("Enter your country : ")
 user_phone_number = input("Enter your phone_number: ")
 user_email = input("Enter your email: ")
 print()
 
 #intantiate
-my_details = Users(user_first_name, user_middle_name, user_last_name, user_age, user_country_code, user_phone_number,
-                   user_email)
+my_details = Users(user_first_name, user_middle_name, user_last_name, user_age, user_phone_number, user_country, user_email)
 my_details.users_name()
 my_details.users_age()
 my_details.users_phone_number()
